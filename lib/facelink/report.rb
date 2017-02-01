c43d7ffa34f7841325342cd6da278686e6cbfbd9
Pedro Vitti <pedrovitti@gmail.com>
2017-02-01 17:02:57 +0100

    Generate CSV file with user interactions for given page ids.
    Move report spec to config spec.

--- lib/facelink/report.rb
+++ lib/facelink/report.rb
@@ -1,14 +1,29 @@
+require 'csv'
+
 module Facelink
 
   class Report
 
-    attr_accessor :file
+    attr_reader :file
 
     def initialize(file)
       @file = file
     end
 
-    def generate
+    def generate_csv
+      csv_file = CSV.open(filepath, 'w') do |csv|
+        CSV.foreach(file) do |row|
+          page_id = row[0]
+          limit = row[1]
+          interactions = Facelink::Client.new.interactions_for(page_id, limit)
+          interactions.each { |interaction| csv << interaction.values }
+        end
+      end
+    end
+
+    def filepath
+      input_file = File.basename(file.path, ".*")
+      "#{File.dirname(file.path)}/#{input_file}-user-interactions.csv"
     end
   end
 
---
+++ spec/facelink/report_spec.rb
@@ -0,0 +1,23 @@
+require "spec_helper"
+
+describe Facelink::Report do
+  let(:file) { File.open(File.join("spec", "fixtures", "input-file.csv")) }
+  let(:report) { described_class.new(file) }
+
+  describe ".generate_csv" do
+    before do
+      facebook_data = JSON.load(File.read(File.join("spec", "fixtures", "stagelink.json")))
+      allow_any_instance_of(Koala::Facebook::API).to receive(:get_connections).and_return(facebook_data)
+    end
+
+    subject { report.generate_csv }
+
+    it "generates a csv file" do
+      subject
+
+      expect(File.read(report.filepath)).to_not be_nil
+    end
+
+  end
+
+end

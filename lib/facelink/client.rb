7998fca42e91f05bb9c868036513bd7647069a6c
Pedro Vitti <pedrovitti@gmail.com>
2017-02-02 07:50:50 +0100

    Refactor reactions and comments method.
    Reuse and extraction of common behaviour.

--- lib/facelink/client.rb
+++ lib/facelink/client.rb
@@ -26,44 +26,25 @@ module Facelink
     def reactions(post)
       reactions_data = Koala::Facebook::API::GraphCollection.new(post["reactions"], graph) || []
 
-      @reactions = transform_data(reactions_data, post)
+      @reactions = transform_data(reactions_data, post, "reaction")
       reactions_data = reactions_data.next_page
 
       while reactions_data
-        @reactions += transform_data(reactions_data, post)
+        @reactions += transform_data(reactions_data, post, "reaction")
         reactions_data = reactions_data.next_page
       end
 
       @reactions
     end
 
-
     def comments(post)
       comments_data = Koala::Facebook::API::GraphCollection.new(post["comments"], graph) || []
 
-      @comments = comments_data.map do |comment|
-        {
-          user_id: comment["from"]["id"],
-          page_id: page_id,
-          post_id: post["id"],
-          post_type: post["type"],
-          interaction_type: "comment"
-        }
-      end
-
-       comments_data = comments_data.next_page
+      @comments = transform_data(comments_data, post, "comment")
+      comments_data = comments_data.next_page
 
       while comments_data
-        @comments = comments_data.map do |comment|
-          {
-            user_id: comment["from"]["id"],
-            page_id: page_id,
-            post_id: post["id"],
-            post_type: post["type"],
-            interaction_type: "comment"
-          }
-        end
-
+        @comments += transform_data(comments_data, post, "comment")
         comments_data = comments_data.next_page
       end
 
@@ -76,16 +57,28 @@ module Facelink
 
     private
 
-    def transform_data(reactions_data, post)
-      reactions_data.map do |reaction|
-        {
-          user_id: reaction["id"],
+    def transform_data(data, post, interaction_type)
+      data.map do |interaction|
+        formatted_data  = {
+          user_id: interaction["id"],
           page_id: page_id,
           post_id: post["id"],
           post_type: post["type"],
-          interaction_type: "reaction",
-          interaction_subtype: reaction["type"]
+          interaction_type: interaction_type
         }
+
+        formatted_data.merge(transform_data_for(interaction_type, interaction))
+      end
+    end
+
+    def transform_data_for(interaction_type, interaction)
+      case interaction_type
+      when "comment"
+        { user_id: interaction["from"]["id"] }
+      when "reaction"
+        { user_id: interaction["id"], interaction_subtype: interaction["type"] }
+      else
+        {}
       end
     end
 
